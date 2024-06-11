316f31ae40808c916e677926e72ed7c0232fb5aa
Grisha <grisha765@tuta.io>
2024-06-11 23:59:50 +0300

    [u] update gpt3

--- providers/ddgo/gpt3.py
+++ providers/ddgo/gpt3.py
@@ -1,45 +1,12 @@
 import asyncio
 
+from providers.ddgo.clicks import clickss
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-# Функция для создания скриншота
-async def take_screenshot(page, file_name):
-    logging.info(f"Taking screenshot: {file_name}")
-    await page.screenshot({'path': file_name})
-    print(f"Screenshot saved as {file_name}")
-
-async def clicks(page, message):
-    textarea = await page.querySelector('textarea')
-    text_content = await page.evaluate('(textarea) => textarea.placeholder', textarea)
-    if 'GPT-3.5' not in text_content:
-        logging.debug("Clicking 'Change provider' button...")
-        buttons = await page.querySelectorAll('[data-reach-tooltip-trigger]')
-        await buttons[0].click()
-        logging.debug("Clicking 'Change to GPT-3.5' button...")
-        await page.waitForSelector('label[for*="gpt-3.5"]')
-        label = await page.querySelector('label[for*="gpt-3.5"]')
-        await label.click()
-        logging.debug("Clicking 'Start New Chat' button...")
-        buttons = await page.querySelectorAll('button')
-        for button in buttons:
-            text = await page.evaluate('(button) => button.textContent', button)
-            if 'Start New Chat' in text:
-                await button.click()
-                break
-
-    # Ввод текста в поле ввода
-    logging.debug(f"Typing text: {message}")
-    await page.type('textarea[placeholder="Chat with GPT-3.5"]', message)
-    
-    # Нажимаем кнопку отправки
-    logging.debug("Clicking 'Send' button...")
-    send_button = await page.querySelector('button[aria-label="Send"]')
-    await send_button.click()
-
 # Функция для отправки сообщения и получения ответа
 async def send_message(page, message, screenshot_path=None):
-    await clicks(page, message)
+    await clickss(page, message, "GPT-3.5", True)
     # Динамическое ожидание появления нового ответа
     logging.debug("Waiting for response...")
     last_response = ""
@@ -62,7 +29,7 @@ async def send_message(page, message, screenshot_path=None):
 
 # Функция для FastAPI, возвращающая ответ без печати
 async def send_message_fastapi(page, message):
-    await clicks(page, message)
+    await clickss(page, message, "GPT-3.5", True)
     # Динамическое ожидание появления нового ответа
     logging.info("Waiting for response...")
     last_response = ""
@@ -71,12 +38,15 @@ async def send_message_fastapi(page, message):
         response_elements = await page.querySelectorAll('div > div:nth-child(2) > div:nth-child(2)')
         if response_elements:
             current_response = await page.evaluate('(element) => element.innerText', response_elements[-1])
+            if "Generating response..." in current_response:
+                continue
             if current_response != last_response:
                 last_response = current_response
             else:
                 break
 
     # Нажимаем кнопку очистки
+    await asyncio.sleep(1)
     buttons = await page.querySelectorAll('[data-reach-tooltip-trigger]')
     if len(buttons) >= 4:
         await buttons[3].click()
