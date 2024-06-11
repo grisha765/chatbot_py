c52fc20c024c4e2aae1d0b9a6a6de2a7fe16e3c9
Grisha <grisha765@tuta.io>
2024-06-11 16:17:19 +0300

    [u] update logs

--- config/logging_config.py
+++ config/logging_config.py
@@ -1,5 +1,7 @@
 import logging
+import os
 
+from config.config import Config 
 # Определяем ANSI escape-коды для цветов
 RESET = "\x1b[0m"
 WHITE = "\x1b[0m"
@@ -15,19 +17,17 @@ class ColoredFormatter(logging.Formatter):
     def format(self, record):
         log_color = COLORS.get(record.levelname, RESET)
         message = super().format(record)
-        return f"{log_color}{record.levelname}{RESET}{WHITE}: {message}{RESET}"
+        # Извлечение имени файла без подчеркиваний
+        filename = os.path.splitext(os.path.basename(record.pathname))[0]
+        return f"{log_color}{record.levelname}{RESET}{WHITE}: {filename}: {message}{RESET}"
 
-def setup_logging(name='my_app', level=logging.DEBUG, enable_detailed_logs=True):
+def setup_logging(name='my_app'):
     logger = logging.getLogger(name)
     if not logger.handlers:  # Проверяем, есть ли уже обработчики у логгера
         handler = logging.StreamHandler()
-        formatter = ColoredFormatter('    %(message)s')
+        formatter = ColoredFormatter('%(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
-        if enable_detailed_logs:
-            logger.setLevel(level)
-        else:
-            logger.setLevel(logging.INFO) # Уровень лога
+        logger.setLevel(getattr(logging, Config.log_level, logging.INFO))  # Устанавливаем уровень логирования явно
     return logger
-
 
