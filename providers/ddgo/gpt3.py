f2cd4ae7972838c3ce545f8e28b4361c497ff2c4
Grisha <grisha765@tuta.io>
2024-06-11 22:24:04 +0300

    [u] add check provider

--- providers/ddgo/gpt3.py
+++ providers/ddgo/gpt3.py
@@ -3,8 +3,31 @@ import asyncio
 from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
-# Функция для отправки сообщения и получения ответа
-async def send_message(page, message, screenshot_path=None):
+# Функция для создания скриншота
+async def take_screenshot(page, file_name):
+    logging.info(f"Taking screenshot: {file_name}")
+    await page.screenshot({'path': file_name})
+    print(f"Screenshot saved as {file_name}")
+
+async def clicks(page, message):
+    textarea = await page.querySelector('textarea')
+    text_content = await page.evaluate('(textarea) => textarea.placeholder', textarea)
+    if 'GPT-3.5' not in text_content:
+        logging.debug("Clicking 'Change provider' button...")
+        buttons = await page.querySelectorAll('[data-reach-tooltip-trigger]')
+        await buttons[0].click()
+        logging.debug("Clicking 'Change to GPT-3.5' button...")
+        await page.waitForSelector('label[for*="gpt-3.5"]')
+        label = await page.querySelector('label[for*="gpt-3.5"]')
+        await label.click()
+        logging.debug("Clicking 'Start New Chat' button...")
+        buttons = await page.querySelectorAll('button')
+        for button in buttons:
+            text = await page.evaluate('(button) => button.textContent', button)
+            if 'Start New Chat' in text:
+                await button.click()
+                break
+
     # Ввод текста в поле ввода
     logging.debug(f"Typing text: {message}")
     await page.type('textarea[placeholder="Chat with GPT-3.5"]', message)
@@ -13,7 +36,10 @@ async def send_message(page, message, screenshot_path=None):
     logging.debug("Clicking 'Send' button...")
     send_button = await page.querySelector('button[aria-label="Send"]')
     await send_button.click()
-    
+
+# Функция для отправки сообщения и получения ответа
+async def send_message(page, message, screenshot_path=None):
+    await clicks(page, message)
     # Динамическое ожидание появления нового ответа
     logging.debug("Waiting for response...")
     last_response = ""
@@ -27,26 +53,16 @@ async def send_message(page, message, screenshot_path=None):
             else:
                 break
     
-    # Печатаем ответ
-    print(f"GPT-3.5: {last_response}")
-
     # Делаем скриншот, если указан путь
     if screenshot_path:
         logging.info(f"Saving screenshot to {screenshot_path}")
         await page.screenshot({'path': screenshot_path})
+
     return last_response
 
 # Функция для FastAPI, возвращающая ответ без печати
 async def send_message_fastapi(page, message):
-    # Ввод текста в поле ввода
-    logging.debug(f"Typing text: {message}")
-    await page.type('textarea[placeholder="Chat with GPT-3.5"]', message)
-    
-    # Нажимаем кнопку отправки
-    logging.debug("Clicking 'Send' button...")
-    send_button = await page.querySelector('button[aria-label="Send"]')
-    await send_button.click()
-    
+    await clicks(page, message)
     # Динамическое ожидание появления нового ответа
     logging.info("Waiting for response...")
     last_response = ""
@@ -71,10 +87,5 @@ async def send_message_fastapi(page, message):
     # Возвращаем ответ
     return last_response
 
-# Функция для создания скриншота
-async def take_screenshot(page, file_name):
-    logging.info(f"Taking screenshot: {file_name}")
-    await page.screenshot({'path': file_name})
-    print(f"Screenshot saved as {file_name}")
 if __name__ == "__main__":
     raise RuntimeError("This module should be run only via main.py")
