e3e7635a768d74233e87b995db5cee98946431fe
Grisha <grisha765@tuta.io>
2024-06-11 17:53:26 +0300

    [u] update tests

--- tests/run.py
+++ tests/run.py
@@ -1,10 +1,12 @@
 from tests.test_ddgo import test_bot_response
+from tests.test_fastapi import test_chat_completions
 from config import logging_config
 async def run():
     logging = logging_config.setup_logging(__name__)
     try:
         logging.info("Start tests...")
         await test_bot_response()
+        await test_chat_completions()
     except Exception as e:
         logging.error(f"An error occurred during test execution: {e}")
     else:
