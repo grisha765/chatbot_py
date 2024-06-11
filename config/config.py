b384fbf3f072aaa8f0a993b5fbc2e13b0b0d1021
Grisha <grisha765@tuta.io>
2024-06-11 21:53:20 +0300

    [u] add env

--- config/config.py
+++ config/config.py
@@ -1,11 +1,12 @@
+import os
 class Config():
-    api_host: str = '127.0.0.1'
-    api_port: int = 8000
-    log_level: str = "DEBUG"
+    api_host: str = os.getenv('API_HOST', '127.0.0.1')
+    api_port: int = int(os.getenv('API_PORT', 8000))
+    log_level: str = os.getenv('LOG_LEVEL', "DEBUG")
     api_keys = {
         "adminapi007": "admin",
         "userapi008": "user"
         # Добавьте другие API ключи здесь
     }
-    test_request: str = "Напиши мне слово Тест и ничего более!"
-    test_response: str = "Тест"
+    test_request: str = os.getenv('TEST_REQUEST', "Напиши мне слово Тест и ничего более!")
+    test_response: str = os.getenv('TEST_RESPONSE', "Тест")
