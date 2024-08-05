3c53e7ef7d3ea16167d834f5a3c3cf35013db1cd
Grisha <grisha765@tuta.io>
2024-08-05 21:36:03 +0300

    [u] add temp atribute in func

--- chatbot/providers/ddgo/gpt3.py
+++ chatbot/providers/ddgo/gpt3.py
@@ -5,7 +5,7 @@ from chatbot.config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 # Функция для отправки сообщения и получения ответа
-async def send_message(page, message, screenshot_path=None):
+async def send_message(page, message, screenshot_path=None, temp=False):
     await clickss(page, message, "GPT-4o", True)
     # Динамическое ожидание появления нового ответа
     logging.debug("Waiting for response...")
@@ -19,42 +19,22 @@ async def send_message(page, message, screenshot_path=None):
                 last_response = current_response
             else:
                 break
+
+    # Нажимаем кнопку очистки если выключен temp
+    if temp == False:
+        await asyncio.sleep(1)
+        buttons = await page.query_selector_all('[data-reach-tooltip-trigger]')
+        if len(buttons) >= 5:
+            await buttons[4].click()
+            logging.debug("Clicking 'Clear' button...")
+        else:
+            logging.error("The required button was not found.")
     
     # Делаем скриншот, если указан путь
     if screenshot_path:
         logging.info(f"Saving screenshot to {screenshot_path}")
         await page.screenshot(path=screenshot_path)
 
-    return last_response
-
-# Функция для FastAPI, возвращающая ответ без печати
-async def send_message_fastapi(page, message):
-    await clickss(page, message, "GPT-4o", True)
-    # Динамическое ожидание появления нового ответа
-    logging.info("Waiting for response...")
-    last_response = ""
-    while True:
-        await asyncio.sleep(1)
-        response_elements = await page.query_selector_all('div > div:nth-child(2) > div:nth-child(2)')
-        if response_elements:
-            current_response = await response_elements[-1].inner_text()
-            if "Generating response..." in current_response:
-                continue
-            if current_response != last_response:
-                last_response = current_response
-            else:
-                break
-
-    # Нажимаем кнопку очистки
-    await asyncio.sleep(1)
-    buttons = await page.query_selector_all('[data-reach-tooltip-trigger]')
-    if len(buttons) >= 4:
-        await buttons[3].click()
-        logging.debug("Clicking 'Clear' button...")
-    else:
-        logging.error("The required button was not found.")
-
-    # Возвращаем ответ
     return last_response
 
 if __name__ == "__main__":
--- chatbot/providers/ddgo/llama3.py
+++ chatbot/providers/ddgo/llama3.py
@@ -5,31 +5,8 @@ from chatbot.config import logging_config
 
 logging = logging_config.setup_logging(__name__)
 
-# Функция для отправки сообщения и получения ответа
-async def send_message(page, message, screenshot_path=None):
-    await clickss(page, message, "Llama", False)
-    # Динамическое ожидание появления нового ответа
-    logging.debug("Waiting for response...")
-    last_response = ""
-    while True:
-        await asyncio.sleep(1)
-        response_elements = await page.query_selector_all('div > div:nth-child(2) > div:nth-child(2)')
-        if response_elements:
-            current_response = await response_elements[-1].inner_text()
-            if current_response != last_response:
-                last_response = current_response
-            else:
-                break
-    
-    # Делаем скриншот, если указан путь
-    if screenshot_path:
-        logging.info(f"Saving screenshot to {screenshot_path}")
-        await page.screenshot(path=screenshot_path)
-
-    return last_response
-
 # Функция для FastAPI, возвращающая ответ без печати
-async def send_message_fastapi(page, message):
+async def send_message(page, message, screenshot_path=None, temp=False):
     await clickss(page, message, "Llama", False)
 
     async def wait_for_response(page):
@@ -58,21 +35,27 @@ async def send_message_fastapi(page, message):
         else:
             break
 
-    # Нажимаем кнопку очистки
-    while True:
-        await asyncio.sleep(1)
-        buttons = await page.query_selector_all('[data-reach-tooltip-trigger]')
-        if len(buttons) >= 4:
-            await buttons[3].click()
-            logging.debug("Clicking 'Clear' button...")
-            break
-        else:
-            logging.error("The required button was not found.")
-            await page.reload()
-            await clickss(page, message, "Llama", False)
-            last_response = await wait_for_response(page)
-            if "DuckDuckGo AI Chat is temporarily unavailable. Please refresh the page and try again." not in last_response:
-                continue
+    # Нажимаем кнопку очистки если выключен temp
+    if temp == False:
+        while True:
+            await asyncio.sleep(1)
+            buttons = await page.query_selector_all('[data-reach-tooltip-trigger]')
+            if len(buttons) >= 5:
+                await buttons[4].click()
+                logging.debug("Clicking 'Clear' button...")
+                break
+            else:
+                logging.error("The required button was not found.")
+                await page.reload()
+                await clickss(page, message, "Llama", False)
+                last_response = await wait_for_response(page)
+                if "DuckDuckGo AI Chat is temporarily unavailable. Please refresh the page and try again." not in last_response:
+                    continue
+
+    # Делаем скриншот, если указан путь
+    if screenshot_path:
+        logging.info(f"Saving screenshot to {screenshot_path}")
+        await page.screenshot(path=screenshot_path)
 
     # Возвращаем ответ
     return last_response
